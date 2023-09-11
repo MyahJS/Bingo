d6b123b2bf38c54840f0234eb735523d8e0f8038
Myah Jackson-Solomon <myahj1@umbc.edu>
2023-09-11 12:10:32 -0400

    adding to mytest main

--- mytest.cpp
+++ mytest.cpp
@@ -104,18 +104,18 @@ if (obj1.initCard()){
     else
         cout << "Test msg: Assignment operator test for normal case failed!\n" << endl;
 
-    // // Re-initialize the object to a card of 10x5
-    // cout << "Following is the result of re-initializing the object and re-play:\n" << endl;
-    // obj1.clear();
-    // if (obj1.reCreateCard(2*CARDROWS,CARDCOLS,MINVAL,MAXVAL)){
-    //     obj1.initCard();
-    //     obj1.dumpCard();
-    //     balls = obj1.drawBalls();
-    //     cout << "\ngame over after " << obj1.play(BALLS,balls) << " hits!\n" << endl;
-    //     obj1.dumpCard();
-    // }
+    // Re-initialize the object to a card of 10x5
+    cout << "Following is the result of re-initializing the object and re-play:\n" << endl;
+    obj1.clear();
+    if (obj1.reCreateCard(2*CARDROWS,CARDCOLS,MINVAL,MAXVAL)){
+        obj1.initCard();
+        obj1.dumpCard();
+        balls = obj1.drawBalls();
+        cout << "\ngame over after " << obj1.play(BALLS,balls) << " hits!\n" << endl;
+        obj1.dumpCard();
+    }
     
-    // cout << "The following is the dump of object created by overloaded assignment operator:\n" << endl;
-    // obj2.dumpCard();
+    cout << "The following is the dump of object created by overloaded assignment operator:\n" << endl;
+    obj2.dumpCard();
     return 0;
 }
