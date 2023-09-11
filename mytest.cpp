b81cf605dacb8f81d273070a68715d354090dfba
Myah Jackson-Solomon <myahj1@umbc.edu>
2023-09-11 16:24:44 -0400

    setting up for office hours

--- mytest.cpp
+++ mytest.cpp
@@ -12,11 +12,11 @@ class Tester{
         for (int i=0;i<rhs.m_numRows;i++){
             for (int j=0;j<rhs.m_numCols;j++){
                 result = result && (lhs.m_card[i][j] == rhs.m_card[i][j]);
-                if ((lhs.m_card[i][j] == rhs.m_card[i][j]) == false){
-                    cout << "card cell not the same" << endl;
-                } else {
-                    cout << "passed" << endl;
-                }
+                // if ((lhs.m_card[i][j] == rhs.m_card[i][j]) == false){
+                //     cout << "card cell not the same" << endl;
+                // } else {
+                //     cout << "passed" << endl;
+                // }
             }
         }
 
@@ -24,33 +24,33 @@ class Tester{
         //      m_helper carry the same cell information (exact same copy)
         for (int i=0;i<rhs.m_helperSize;i++){
             result = result && (lhs.m_helper[i] == rhs.m_helper[i]);
-            if ((lhs.m_helper[i] == rhs.m_helper[i]) == false){
-                    cout << "helper cell not the same" << endl;
-            } else {
-                cout << "passed" << endl;
-            }
+            // if ((lhs.m_helper[i] == rhs.m_helper[i]) == false){
+            //         cout << "helper cell not the same" << endl;
+            // } else {
+            //     cout << "passed" << endl;
+            // }
         }
 
         // we expect that the corresponding cells in lhs and rhs
         //      m_trackRows carry the same cell information (exact same copy)
         for (int i=0;i<rhs.m_numRows;i++){
             result = result && (lhs.m_trackRows[i] == rhs.m_trackRows[i]);
-            if ((lhs.m_trackRows[i] == rhs.m_trackRows[i]) == false){
-                    cout << "track rows value not the same" << endl;
-            } else {
-                cout << "passed" << endl;
-            }
+            // if ((lhs.m_trackRows[i] == rhs.m_trackRows[i]) == false){
+            //         cout << "track rows value not the same" << endl;
+            // } else {
+            //     cout << "passed" << endl;
+            // }
         }
 
         // we expect that the corresponding cells in lhs and rhs
         //      m_trackCols carry the same cell information (exact same copy)
         for (int i=0;i<rhs.m_numCols;i++){
             result = result && (lhs.m_trackCols[i] == rhs.m_trackCols[i]);
-            if ((lhs.m_trackCols[i] == rhs.m_trackCols[i]) == false){
-                    cout << "track columns value not the same" << endl;
-            } else {
-                cout << "passed" << endl;
-            }
+            // if ((lhs.m_trackCols[i] == rhs.m_trackCols[i]) == false){
+            //         cout << "track columns value not the same" << endl;
+            // } else {
+            //     cout << "passed" << endl;
+            // }
         }
 
         result = result && (lhs.m_minBallVal == rhs.m_minBallVal);
@@ -88,7 +88,7 @@ int main(){
     Bingo obj1(CARDROWS,CARDCOLS,MINVAL,MAXVAL);
     vector<int> balls = obj1.drawBalls();
 
-if (obj1.initCard()){
+    if (obj1.initCard()){
         obj1.dumpCard();
         cout << "\ngame over after " << obj1.play(BALLS,balls) << " hits!\n" << endl;
         obj1.dumpCard();
@@ -96,26 +96,13 @@ if (obj1.initCard()){
 
     Bingo obj2;
     obj2 = obj1;
+    // test whether the assignment operator normal case works correctly
+    cout << "Following is the test message for testing assignment operator:\n" << endl;
+    if (tester.assignmentNormal(obj2,obj1))
+        cout << "Test msg: Assignment operator test for normal case passed!\n" << endl;
+    else
+        cout << "Test msg: Assignment operator test for normal case failed!\n" << endl;
 
-    // // test whether the assignment operator normal case works correctly
-    // cout << "Following is the test message for testing assignment operator:\n" << endl;
-    // if (tester.assignmentNormal(obj2,obj1))
-    //     cout << "Test msg: Assignment operator test for normal case passed!\n" << endl;
-    // else
-    //     cout << "Test msg: Assignment operator test for normal case failed!\n" << endl;
 
-    // // Re-initialize the object to a card of 10x5
-    // cout << "Following is the result of re-initializing the object and re-play:\n" << endl;
-    // obj1.clear();
-    // if (obj1.reCreateCard(2*CARDROWS,CARDCOLS,MINVAL,MAXVAL)){
-    //     obj1.initCard();
-    //     obj1.dumpCard();
-    //     balls = obj1.drawBalls();
-    //     cout << "\ngame over after " << obj1.play(BALLS,balls) << " hits!\n" << endl;
-    //     obj1.dumpCard();
-    // }
-    
-    // cout << "The following is the dump of object created by overloaded assignment operator:\n" << endl;
-    // obj2.dumpCard();
     return 0;
 }
